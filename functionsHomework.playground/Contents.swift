6ee013e9677dab73cae3ce952d55997d8e2fa283
Alex Zavadskiy <fh.zavadskii@gmail.com>
2023-01-10 20:48:12 +0300

    replaced assert method in 2-4 functions with XCTest framework, add comments

--- functionsHomework.playground/Contents.swift
+++ functionsHomework.playground/Contents.swift
@@ -1,133 +1,97 @@
 import XCTest
 
-// MARK: - Choose min between 2 Int
-
-func minimumOfTwo(_ number1: Int, _ number2: Int) -> Int {
-    (number1 < number2) ? number1 : number2
-}
-
-assert(minimumOfTwo(1, 2) == 1)
-assert(minimumOfTwo(-2, 2) == -2)
-assert(minimumOfTwo(0, 0) == 0)
-assert(minimumOfTwo(Int.min, Int.min) == Int.min)
-
 // MARK: - Choose max between 2 Int
 
+/// Return the greater value from 2 Int input.
 func maximumOfTwo(_ number1: Int, _ number2: Int) -> Int {
     (number1 > number2) ? number1 : number2
 }
 
-assert(maximumOfTwo(1, 2) == 2)
-assert(maximumOfTwo(-2, 2) == 2)
-assert(maximumOfTwo(0, 0) == 0)
-assert(maximumOfTwo(Int.max, Int.max) == Int.max)
+/// Tests for maximumOfTwo function.
+class MaximumOfTwoTests: XCTestCase {
+    /// Test case for situation with input of two positive Int values.
+    func testFindMaxValueReturnsMaxValueWhenGiveTwoPositiveNumbers() {
+        XCTAssertEqual(maximumOfTwo(1, 2), 2)
+    }
+
+    /// Test case for situation with input of one positive and one negative Int value.
+    func testFindMaxValueReturnsMaxValueWhenGiveOneNegavtiveAndOnePositiveNumber() {
+        XCTAssertEqual(maximumOfTwo(-2, 2), 2)
+    }
+
+    /// Test case for situation with input of two zero values.
+    func testFindMaxValueReturnsMaxValueWhenGiveTwoZeros() {
+        XCTAssertEqual(maximumOfTwo(0, 0), 0)
+    }
+
+    /// Test case for situation with input of two minimum representable integer values.
+    func testFindMaxValueReturnsMaxValueWhenGiveTwoIntMaxNumbers() {
+        XCTAssertEqual(maximumOfTwo(Int.max, Int.max), Int.max)
+    }
+}
+
+MaximumOfTwoTests.defaultTestSuite.run()
 
 // MARK: - Choose min between 3 Int
 
+/// Return the least value from 3 Int input.
 func minimumOfThree(_ number1: Int, _ number2: Int, _ number3: Int) -> Int {
     minimumOfTwo(number3, minimumOfTwo(number1, number2))
 }
 
-assert(minimumOfThree(1, 2, 3) == 1)
-assert(minimumOfThree(-6, 6, 0) == -6)
-assert(minimumOfThree(0, 0, 1) == 0)
-assert(minimumOfThree(0, 0, 0) == 0)
+/// Tests for minimumOfThree function.
+class MinimumOfThreeTests: XCTestCase {
+    /// Test case for situation with input of three positive Int values.
+    func testFindMinValueReturnsMinValueWhenGiveThreePositiveNumbers() {
+        XCTAssertEqual(minimumOfThree(1, 2, 3), 1)
+    }
+
+    /// Test case for situation with input of one positive and one negative and one zero Int value.
+    func testFindMinValueReturnsMinValueWhenGiveThreeDiffernetNumbers() {
+        XCTAssertEqual(minimumOfThree(-6, 6, 0), -6)
+    }
+
+    /// Test case for situation with input of two zero values and one positive Int value.
+    func testFindMinValueReturnsMinValueWhenGiveTwoZerosAndOnePositiveNumbers() {
+        XCTAssertEqual(minimumOfThree(0, 0, 1), 0)
+    }
+
+    /// Test case for situation with input of three zero values.
+    func testFindMinValueReturnsMinValueWhenGiveThreeZeros() {
+        XCTAssertEqual(minimumOfThree(0, 0, 0), 0)
+    }
+}
+
+MinimumOfThreeTests.defaultTestSuite.run()
 
 // MARK: - Choose max between 3 Int
 
+/// Return the greater value from 3 Int input.
 func maximumOfThree(_ number1: Int, _ number2: Int, _ number3: Int) -> Int {
     maximumOfTwo(number3, maximumOfTwo(number1, number2))
 }
 
-assert(maximumOfThree(1, 2, 3) == 3)
-assert(maximumOfThree(-6, 6, 0) == 6)
-assert(maximumOfThree(0, 0, 1) == 1)
-assert(maximumOfThree(0, 0, 0) == 0)
-
-// MARK: - Find min in array of Ints
-
-func minimumNumberInArrayOfInt(_ array: [Int]) -> Int? {
-    guard var minimumNumber = array.first else {
-        return nil
+/// Tests for maximumOfThree function.
+class MaximumOfThreeTests: XCTestCase {
+    /// Test case for situation with input of three positive Int values.
+    func testFindMinValueReturnsMinValueWhenGiveThreePositiveNumbers() {
+        XCTAssertEqual(maximumOfThree(1, 2, 3), 3)
     }
-    
-    for item in array {
-        minimumNumber = minimumOfTwo(item, minimumNumber)
+
+    /// Test case for situation with input of one positive and one negative and one zero Int value.
+    func testFindMinValueReturnsMinValueWhenGiveThreeDiffernetNumbers() {
+        XCTAssertEqual(maximumOfThree(-6, 6, 0), 6)
     }
-    
-    return minimumNumber
+
+    /// Test case for situation with input of two zero values and one positive Int value.
+    func testFindMinValueReturnsMinValueWhenGiveTwoZerosAndOnePositiveNumbers() {
+        XCTAssertEqual(maximumOfThree(0, 0, 1), 1)
+    }
+
+    /// Test case for situation with input of three zero values.
+    func testFindMinValueReturnsMinValueWhenGiveThreeZeros() {
+        XCTAssertEqual(maximumOfThree(0, 0, 0), 0)
+    }
 }
 
-assert(minimumNumberInArrayOfInt([3, 2, 1]) == 1)
-assert(minimumNumberInArrayOfInt([]) == nil)
-assert(minimumNumberInArrayOfInt([2]) == 2)
-assert(minimumNumberInArrayOfInt([0, 0, 0]) == 0)
-assert(minimumNumberInArrayOfInt([-2, 2]) == -2)
-
-// MARK: - Find max in array of Ints
-
-func maximumNumberInArrayOfInt(_ array: [Int]) -> Int? {
-    guard var maximumNumber = array.first else {
-        return nil
-    }
-    
-    for item in array {
-        maximumNumber = maximumOfTwo(item, maximumNumber)
-    }
-    
-    return maximumNumber
-}
-
-assert(maximumNumberInArrayOfInt([1, 2, 3]) == 3)
-assert(maximumNumberInArrayOfInt([]) == nil)
-assert(maximumNumberInArrayOfInt([2]) == 2)
-assert(maximumNumberInArrayOfInt([0, 0, 0]) == 0)
-assert(maximumNumberInArrayOfInt([-2, 2]) == 2)
-
-// MARK: - Find min in array of Doubles
-
-func minimumNumberInArrayOfDouble(_ array: [Double]) -> Double? {
-    guard var minimumNumber = array.first else {
-        return nil
-    }
-    
-    for item in array {
-        if item < minimumNumber {
-            minimumNumber = item
-        }
-    }
-    
-    return minimumNumber
-}
-
-assert(minimumNumberInArrayOfDouble([3.1, 3.2, 4]) == 3.1)
-assert(minimumNumberInArrayOfDouble([3.1, 3.2, 4.4]) == 3.1)
-assert(minimumNumberInArrayOfDouble([3.33, 3.32, 3.31]) == 3.31)
-assert(minimumNumberInArrayOfDouble([]) == nil)
-assert(minimumNumberInArrayOfDouble([2.1]) == 2.1)
-assert(minimumNumberInArrayOfDouble([0, 0, 0]) == 0)
-assert(minimumNumberInArrayOfDouble([3, 4, 5]) == 3)
-
-// MARK: - Find max in array of Doubles
-
-func maximumNumberInArrayOfDouble(_ array: [Double]) -> Double? {
-    guard var maximumNumber = array.first else {
-        return nil
-    }
-    
-    for item in array {
-        if item > maximumNumber {
-            maximumNumber = item
-        }
-    }
-    
-    return maximumNumber
-}
-
-assert(maximumNumberInArrayOfDouble([3.1, 3.2, 4]) == 4)
-assert(maximumNumberInArrayOfDouble([3.1, 3.2, 4.4]) == 4.4)
-assert(maximumNumberInArrayOfDouble([3.33, 3.32, 3.31]) == 3.33)
-assert(maximumNumberInArrayOfDouble([]) == nil)
-assert(maximumNumberInArrayOfDouble([2.1]) == 2.1)
-assert(maximumNumberInArrayOfDouble([0, 0, 0]) == 0)
-assert(maximumNumberInArrayOfDouble([3, 4, 5]) == 5)
+MaximumOfThreeTests.defaultTestSuite.run()

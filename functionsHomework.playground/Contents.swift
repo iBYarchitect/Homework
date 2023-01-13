15f00a3e6b50cbda36f2dbd34fc22591724169b2
Alex Zavadskiy <fh.zavadskii@gmail.com>
2023-01-13 12:57:09 +0300

    return lines of code from main branch and upgrade documentation comments

--- functionsHomework.playground/Contents.swift
+++ functionsHomework.playground/Contents.swift
@@ -1,22 +1,71 @@
 import XCTest
 
+// MARK: - Choose min between 2 Int
+func minimumOfTwo(_ number1: Int, _ number2: Int) -> Int {
+    (number1 < number2) ? number1 : number2
+}
+
+assert(minimumOfTwo(1, 2) == 1)
+assert(minimumOfTwo(-2, 2) == -2)
+assert(minimumOfTwo(0, 0) == 0)
+assert(minimumOfTwo(Int.min, Int.min) == Int.min)
+
+// MARK: - Choose max between 2 Int
+func maximumOfTwo(_ number1: Int, _ number2: Int) -> Int {
+    (number1 > number2) ? number1 : number2
+}
+
+assert(maximumOfTwo(1, 2) == 2)
+assert(maximumOfTwo(-2, 2) == 2)
+assert(maximumOfTwo(0, 0) == 0)
+assert(maximumOfTwo(Int.max, Int.max) == Int.max)
+
+// MARK: - Choose min between 3 Int
+func minimumOfThree(_ number1: Int, _ number2: Int, _ number3: Int) -> Int {
+    minimumOfTwo(number3, minimumOfTwo(number1, number2))
+}
+
+assert(minimumOfThree(1, 2, 3) == 1)
+assert(minimumOfThree(-6, 6, 0) == -6)
+assert(minimumOfThree(0, 0, 1) == 0)
+assert(minimumOfThree(0, 0, 0) == 0)
+
+// MARK: - Choose max between 3 Int
+func maximumOfThree(_ number1: Int, _ number2: Int, _ number3: Int) -> Int {
+    maximumOfTwo(number3, maximumOfTwo(number1, number2))
+}
+
+assert(maximumOfThree(1, 2, 3) == 3)
+assert(maximumOfThree(-6, 6, 0) == 6)
+assert(maximumOfThree(0, 0, 1) == 1)
+assert(maximumOfThree(0, 0, 0) == 0)
+
 // MARK: - Find min in array of Ints
 
-/// Return the least value from array of Int input.
-/// Asymptotic complexity - O(n).
+/**
+    This function chooses minimum value on array of Int values.
+ 
+    You can find minimum value of Int from input array of Int values by calling this function.
+ 
+    - Parameter array: input array of Int values.
+ 
+    - Returns: Returns minimum Int value from input array of Int values or nil if the no elements in array.
+ 
+    - Complexity: This function finds the maximum value in array using O(N) algorithm.
+ 
+    */
 func minimumNumberInArrayOfInt(_ array: [Int]) -> Int? {
     guard var minimumNumber = array.first else {
         return nil
     }
 
     for item in array {
-        minimumNumber = min(item, minimumNumber)
+        minimumNumber = minimumOfTwo(item, minimumNumber)
     }
 
     return minimumNumber
 }
 
-/// Tests for minimumNumberInArrayOfInt function.
 class MinimumNumberInArrayOfIntTests: XCTestCase {
     /// Test case for situation with input of positive array Int with three values.
     func testFindMinValueReturnsMinValueWhenGiveArrayOfPositiveInt() {
@@ -48,21 +97,30 @@ MinimumNumberInArrayOfIntTests.defaultTestSuite.run()
 
 // MARK: - Find max in array of Ints
 
-/// Return the greater value from array of Int input.
-/// Asymptotic complexity - O(n).
+/**
+    This function chooses maximum value on array of Int values.
+ 
+    You can find maximum value of Int from input array of Int values by calling this function.
+ 
+    - Parameter array: input array of Int values.
+ 
+    - Returns: Returns maximum Int value from input array of Int values or nil if the no elements in array.
+ 
+    - Complexity: This function finds the maximum value in array using O(N) algorithm.
+ 
+    */
 func maximumNumberInArrayOfInt(_ array: [Int]) -> Int? {
     guard var maximumNumber = array.first else {
         return nil
     }
 
     for item in array {
-        maximumNumber = max(item, maximumNumber)
+        maximumNumber = maximumOfTwo(item, maximumNumber)
     }
 
     return maximumNumber
 }
 
-/// Tests for maximumNumberInArrayOfInt function.
 class MaximumNumberInArrayOfIntTests: XCTestCase {
     /// Test case for situation with input of positive array Int with three values.
     func testFindMaxValueReturnsMinValueWhenGiveArrayOfPositiveInt() {
@@ -94,8 +152,18 @@ MaximumNumberInArrayOfIntTests.defaultTestSuite.run()
 
 // MARK: - Find min in array of Doubles
 
-/// Return the least value from array of Double input.
-/// Asymptotic complexity - O(n).
+/**
+    This function chooses minimum value on array of Double values.
+ 
+    You can find minimum value of Double from input array of Double values by calling this function.
+ 
+    - Parameter array: input array of Double values.
+ 
+    - Returns: Returns minimum Double value from input array of Double values or nil if the no elements in array.
+ 
+    - Complexity: This function finds the maximum value in array using O(N) algorithm.
+ 
+    */
 func minimumNumberInArrayOfDouble(_ array: [Double]) -> Double? {
     guard var minimumNumber = array.first else {
         return nil
@@ -110,7 +178,6 @@ func minimumNumberInArrayOfDouble(_ array: [Double]) -> Double? {
     return minimumNumber
 }
 
-/// Tests for minimumNumberInArrayOfDouble function.
 class MinimumNumberInArrayOfDoubleTests: XCTestCase {
     /// Test case for situation with input positive array of Double with two Double and one Int values.
     func testFindMinValueReturnsMinValueWhenGiveArrayOfTwoPositiveDoublesAndOneIntValues() {
@@ -147,8 +214,18 @@ MinimumNumberInArrayOfDoubleTests.defaultTestSuite.run()
 
 // MARK: - Find max in array of Doubles
 
-/// Return the greater value from array of Double input.
-/// Asymptotic complexity - O(n).
+/**
+    This function chooses maximum value on array of Double values.
+ 
+    You can find maximum value of Double from input array of Double values by calling this function.
+ 
+    - Parameter array: input array of Double values.
+ 
+    - Returns: Returns maximum Double value from input array of Double values or nil if the no elements in array.
+ 
+    - Complexity: This function finds the maximum value in array using O(N) algorithm.
+ 
+    */
 func maximumNumberInArrayOfDouble(_ array: [Double]) -> Double? {
     guard var maximumNumber = array.first else {
         return nil
@@ -163,7 +240,6 @@ func maximumNumberInArrayOfDouble(_ array: [Double]) -> Double? {
     return maximumNumber
 }
 
-/// Tests for maximumNumberInArrayOfDouble function.
 class MaximumNumberInArrayOfDoubleTests: XCTestCase {
     /// Test case for situation with input positive array of Double with two Double and one Int values.
     func testFindMinValueReturnsMinValueWhenGiveArrayOfTwoPositiveDoublesAndOneIntValues() {

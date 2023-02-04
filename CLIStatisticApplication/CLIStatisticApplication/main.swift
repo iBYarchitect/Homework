f3be29f322f9f1e92749c4a3e37204e38cf65d14
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-02-04 14:37:12 +0300

    Delete auto generated lines of code in main.swift

--- CLIStatisticApplication/CLIStatisticApplication/main.swift
+++ CLIStatisticApplication/CLIStatisticApplication/main.swift
@@ -1,11 +1,1 @@
-//
-//  main.swift
-//  CLIStatisticApplication
-//
-//  Created by Alex Zavadskiy on 4.02.23.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("Hello, from CLIStatisticApplication!")

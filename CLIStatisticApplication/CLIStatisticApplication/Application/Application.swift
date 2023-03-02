0c13a370f144211dfc1586deaf7d4f8e17495f62
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-02 14:56:59 +0100

    Adding documentation comment for struct Application.
    Adding documentation comment for struct Application.

--- CLIStatisticApplication/CLIStatisticApplication/Application/Application.swift
+++ CLIStatisticApplication/CLIStatisticApplication/Application/Application.swift
@@ -1,4 +1,10 @@
+/**
+ A model representing the execution of the structure of the application.
+ */
 struct Application {
+    /**
+     ToDo
+     */
     static func run() {
         print("Hello user, this is CLIStatisticApplication!")
     }

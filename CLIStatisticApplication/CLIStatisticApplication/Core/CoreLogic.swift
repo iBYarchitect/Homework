8256bd8c3d952d0601e9b651a790c4bb064e1764
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-03 17:38:25 +0100

    Improvements in the documentation comment for CoreLogic struct.
    Improvements in the documentation comment for CoreLogic struct.

--- CLIStatisticApplication/CLIStatisticApplication/Core/CoreLogic.swift
+++ CLIStatisticApplication/CLIStatisticApplication/Core/CoreLogic.swift
@@ -1,4 +1,4 @@
 /**
- Represents an object which provides methods for calculating the mean, median, and mode value of an array of integers from user input from a shell.
+ Object which provides methods for calculating the mean, median, and mode value of an array of integers from user input from a shell.
  */
 struct CoreLogic {}

2d8b3c5efb98761cd4841ab6b8de0abd8dfd6546
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-03 17:39:51 +0100

    Minor improvements in the documentation comment for Parser struct.
    Minor improvements in the documentation comment for Parser struct.

--- CLIStatisticApplication/CLIStatisticApplication/ArgumentParser/Parser.swift
+++ CLIStatisticApplication/CLIStatisticApplication/ArgumentParser/Parser.swift
@@ -1,4 +1,4 @@
 /**
- Represents an object which allows the parsing of command-line arguments and commands. It provides a simple and convenient method to define the expected arguments and commands and extract them from the command line arguments.
+ Object which allows the parsing of command-line arguments and commands. It provides a simple and convenient method to define the expected arguments and commands and extract them from the command line arguments.
  */
 struct Parser {}

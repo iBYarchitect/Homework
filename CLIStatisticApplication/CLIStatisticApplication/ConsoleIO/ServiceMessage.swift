e946a4eecc57a1f6feb06db2fddcd177d6fe604c
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-03 11:15:25 +0100

    Improvements in the documentation comment for ServiceMessage struct.
    Improvements in the documentation comment for ServiceMessage struct.

--- CLIStatisticApplication/CLIStatisticApplication/ConsoleIO/ServiceMessage.swift
+++ CLIStatisticApplication/CLIStatisticApplication/ConsoleIO/ServiceMessage.swift
@@ -1,4 +1,4 @@
 /**
- A model representing possible text messages for output to user shell in different cases.
+ Contain typical messages as error or help which uses in console output.
  */
 struct ServiceMessage {}

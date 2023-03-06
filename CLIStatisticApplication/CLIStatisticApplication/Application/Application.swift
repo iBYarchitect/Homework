a27b89180ee030f6e322a3784feaa59d2a946356
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-06 21:58:46 +0100

    Adding interactive and non-interactive mod for application method run.
    Adding interactive and non-interactive mod for application method run.

--- CLIStatisticApplication/CLIStatisticApplication/Application/Application.swift
+++ CLIStatisticApplication/CLIStatisticApplication/Application/Application.swift
@@ -6,6 +6,43 @@ struct Application {
      Method that executes the application.
      */
     static func run() {
-        print("Hello user, this is CLIStatisticApplication!")
+        ConsoleOutput().printToShell("Welcome to CLIStatisticApplication, this programm could calculate mean value from number input")
+        let inputFromConsole = ConsoleInput().readFromShell()
+
+        if inputFromConsole.isEmpty {
+            ConsoleOutput().printToShell("Type '--mean' to calculate mean value or '--help' to watch help")
+
+            let input = readLine()
+            switch input {
+            case SupportedCommand.mean.rawValue:
+                if let argument = readLine() {
+                    let nums = argument.split(separator: " ").compactMap { Int($0) }
+                    let result = String(CoreLogic.meanCalculation(nums))
+                    ConsoleOutput().printToShell(result)
+                }
+            case SupportedCommand.help.rawValue:
+                ConsoleOutput().printToShell(ServiceMessages().help)
+            default:
+                ConsoleOutput().printToShell("Error: unknown command in interactive mode, type '--help'")
+            }
+        } else {
+            let parser = Parser()
+            let parsedData = parser.parse(inputFromConsole)
+
+            switch parsedData.command.rawValue {
+            case SupportedCommand.mean.rawValue:
+                if parsedData.data.isEmpty {
+                    ConsoleOutput().printToShell("Invalid argument")
+                } else {
+                    let result = String(CoreLogic.meanCalculation(parsedData.data))
+                    ConsoleOutput().printToShell(result)
+                }
+            case SupportedCommand.help.rawValue:
+                ConsoleOutput().printToShell(ServiceMessages().help)
+            default:
+                let error = "error from App.run.switch"
+                ConsoleOutput().printToShell(error)
+            }
+        }
     }
 }

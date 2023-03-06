a4b823154da144fc88dcb0db24bcc95dda441c92
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-06 22:20:52 +0100

    Adding method meanCalculation in CoreLogic struct.
    Adding method meanCalculation in CoreLogic struct.

--- CLIStatisticApplication/CLIStatisticApplication/Core/CoreLogic.swift
+++ CLIStatisticApplication/CLIStatisticApplication/Core/CoreLogic.swift
@@ -1,4 +1,15 @@
 /**
  Object that implements the main logic of the application, especially the computations of various statistics for the user.
  */
-struct CoreLogic {}
+struct CoreLogic {
+    static func meanCalculation(_ array: [Int]) -> Double {
+        let count = Double(array.count)
+        var sum = 0.0
+
+        for number in array {
+            sum += Double(number)
+        }
+
+        return sum / count
+    }
+}

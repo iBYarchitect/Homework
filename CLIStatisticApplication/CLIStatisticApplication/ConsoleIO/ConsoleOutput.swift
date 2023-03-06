d18edea7ae20efb1e6610ff9fc12cf8abfa252c4
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-06 22:17:43 +0100

    Adding method printToShell in ConsoleOutput struct.
    Adding method printToShell in ConsoleOutput struct.

--- CLIStatisticApplication/CLIStatisticApplication/ConsoleIO/ConsoleOutput.swift
+++ CLIStatisticApplication/CLIStatisticApplication/ConsoleIO/ConsoleOutput.swift
@@ -1,4 +1,12 @@
 /**
  Object that can print output to the shell/console.
  */
-struct ConsoleOutput {}
+struct ConsoleOutput {
+    func printToShell(_ message: String) {
+        if message == "nan" {
+            print("Invalid argument")
+        } else {
+            print(message)
+        }
+    }
+}

fca42548eefbc6a42a5014e54b5c0d42a26dc071
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-06 22:09:49 +0100

    Adding functionality for the parser.
    Adding functionality for the parser.

--- CLIStatisticApplication/CLIStatisticApplication/ArgumentParser/Parser.swift
+++ CLIStatisticApplication/CLIStatisticApplication/ArgumentParser/Parser.swift
@@ -1,4 +1,29 @@
 /**
  Object which allows the parsing of command-line arguments and commands. It provides a simple and convenient method to define the expected arguments and commands and extract them from the command line arguments.
  */
-struct Parser {}
+struct Parser {
+    func parse(_ userInput: [String]) -> ParsedData {
+        let tempCommand = userInput.first
+        var result = ParsedData(command: SupportedCommand.mean, data: [])
+
+        switch tempCommand {
+        case SupportedCommand.mean.rawValue:
+            result.data = convertStringArrayToIntArray(Array(userInput.dropFirst()))
+            result.command = SupportedCommand.mean
+        case SupportedCommand.help.rawValue:
+            result.data = convertStringArrayToIntArray(Array(userInput.dropFirst()))
+            result.command = SupportedCommand.help
+        default:
+            ConsoleOutput().printToShell("Invalid command, please write --help to look at possible commands")
+        }
+
+        return result
+    }
+
+    func convertStringArrayToIntArray(_ stringArray: [String]) -> [Int] {
+        guard !stringArray.isEmpty else { return [] }
+        let intArray = stringArray.compactMap { Int($0) }
+
+        return intArray
+    }
+}

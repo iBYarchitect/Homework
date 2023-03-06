8994b19c9ef808f4d4d793b97aeac17062a869d9
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-06 22:19:40 +0100

    Adding help message in ServiceMessage struct.
    Adding help message in ServiceMessage struct.

--- CLIStatisticApplication/CLIStatisticApplication/ConsoleIO/ServiceMessage.swift
+++ CLIStatisticApplication/CLIStatisticApplication/ConsoleIO/ServiceMessage.swift
@@ -1,4 +1,10 @@
 /**
  Contains different types of messages which uses in console output.
  */
-struct ServiceMessage {}
+struct ServiceMessage {
+    let help = """
+        Possible commands:
+        --mean write this command and sequence of numbers to calculate mean value
+        --help show possible commands
+    """
+}

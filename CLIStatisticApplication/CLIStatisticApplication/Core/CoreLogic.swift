47f1fa06a350024996f524f56c974646c6ba6fff
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-08 12:53:47 +0100

    Adding documentation for method calculateMean in CoreLogic struct.
    Adding documentation for method calculateMean in CoreLogic struct.

--- CLIStatisticApplication/CLIStatisticApplication/Core/CoreLogic.swift
+++ CLIStatisticApplication/CLIStatisticApplication/Core/CoreLogic.swift
@@ -2,6 +2,14 @@
  Object that implements the main logic of the application, especially the computations of various statistics for the user.
  */
 struct CoreLogic {
+    /**
+     Returns mean value.
+     
+     Calculates the mean value from input array of integer values.
+     - Parameter array: array of Int values.
+     - Returns: the mean value in the Double form.
+     - Complexity: O(n), where n is the length of this input array.
+     */
     static func meanCalculation(_ array: [Int]) -> Double {
         let count = Double(array.count)
         var sum = 0.0

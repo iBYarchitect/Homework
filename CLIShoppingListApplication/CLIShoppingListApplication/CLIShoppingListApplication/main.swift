d47ef5c409dca3152689a1eb70ffab4b3f541e8e
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-15 21:18:09 +0100

    Changing string for print in main.swift

--- CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/main.swift
+++ CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/main.swift
@@ -1,11 +1,1 @@
-//
-//  main.swift
-//  CLIShoppingListApplication
-//
-//  Created by Alex Zavadskiy on 15.03.23.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("Hello, from CLIShoppingListApplication!")

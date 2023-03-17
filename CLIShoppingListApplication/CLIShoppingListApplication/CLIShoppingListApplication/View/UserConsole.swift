afa3bbb4f28016cb6182ba39b2f89a35ccbe73f5
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-17 22:50:40 +0100

    Adding methods to UserConsole.

--- CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/View/UserConsole.swift
+++ CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/View/UserConsole.swift
@@ -1,1 +1,20 @@
-struct UserConsole: ConsoleIOWrapper {}
+import Darwin
+
+struct UserConsole: ConsoleIOWrapper {
+    func readLine() -> String {
+        Swift.readLine() ?? ""
+    }
+
+    func write(_ items: Any...) {
+        items.forEach { Swift.print($0) }
+    }
+
+    func writeErrorLine(_ errorMessage: String) {
+        writeErrorLine(errorMessage, "")
+    }
+
+    func writeErrorLine(_ errorMessage: String, _ helpMessage: String) {
+        fputs("\(TextPrefix.error): \(errorMessage)\n", stderr)
+        if !helpMessage.isEmpty { fputs("\(helpMessage)\n", stderr) }
+    }
+}

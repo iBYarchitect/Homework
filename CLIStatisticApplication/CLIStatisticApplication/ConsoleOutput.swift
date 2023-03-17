869a90f256ff80a1b8fc706f9a47706bf86278a3
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-17 23:50:52 +0100

    Return files after deleting.

---
+++ CLIStatisticApplication/CLIStatisticApplication/ConsoleOutput.swift
@@ -0,0 +1,19 @@
+//struct ConsoleOutput {
+//    func printToShell(_ message: String) {
+//        if message == "nan" {
+//            print("Invalid argument")
+//        } else {
+//            print(message)
+//        }
+//    }
+//}
+
+struct ConsoleOutput {
+    func printToShell(_ message: String) {
+        if message == "nan" {
+            print("Invalid argument")
+        } else {
+            print(message)
+        }
+    }
+}
---
+++ CLIStatisticApplication/CLIStatisticApplication/PossibleCommands.swift
@@ -0,0 +1,5 @@
+enum PossibleCommands: String {
+    case mean = "--mean"
+    case help = "--help"
+    case quit = "q"
+}

e80156153bd82b42c078cd906dedec0f201091b8
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-21 16:21:27 +0100

    Fixing documentation in Mediator struct.

--- CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/Controller/Mediator.swift
+++ CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/Controller/Mediator.swift
@@ -1,4 +1,5 @@
 /**
- Object that sends requests to the model and processes this data that is the result of the methods InMemoryShoppingList.
+ Object that sends requests to the model and processes this data that is
+ the result of the methods of the protocol describing the shopping list interface.
  */
 struct Mediator {}

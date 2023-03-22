16d86e69f98489ae193abc7148300a6cce9ace57
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-22 14:24:04 +0100

    Fixes in documentation.

--- CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/Model/InMemoryShoppingList.swift
+++ CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/Model/InMemoryShoppingList.swift
@@ -15,21 +15,21 @@ struct DummyGroceryList: ShoppingList {
     }
     
     /**
-    A stub method to mock behavior of conforming to the protocol `ShoppingList`.
-    - Parameter item: Item to remove from a `shoppingList`.
-    The `shoppingList` won't be modified upon the method's call.
-    - Returns: hardcode `true` value. The returned values does not depend on input data.
-    */
+     A stub method to mock behavior of conforming to the protocol `ShoppingList`.
+     - Parameter item: Item to remove from a `shoppingList`.
+     The `shoppingList` won't be modified upon the method's call.
+     - Returns: hardcode `true` value. The returned values does not depend on input data.
+     */
     func remove(_ item: String) -> Bool {
         true
     }
-
+    
     /**
-    A stub method to mock behavior of conforming to the protocol `ShoppingList`.
-    - Parameter index: The position of the element in the `shoppingList` to remove.
-    The `shoppingList` won't be modified upon the method's call.
-    - Returns: hardcode `true` value. The returned values does not depend on input data.
-    */
+     A stub method to mock behavior of conforming to the protocol `ShoppingList`.
+     - Parameter index: The position of the element in the `shoppingList` to remove.
+     The `shoppingList` won't be modified upon the method's call.
+     - Returns: hardcode `true` value. The returned values does not depend on input data.
+     */
     func remove(at index: Int) -> Bool {
         true
     }
--- CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/View/UserConsole.swift
+++ CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/View/UserConsole.swift
@@ -8,15 +8,15 @@ struct UserConsole: ConsoleIOWrapper {
     func readLine() -> String {
         Swift.readLine() ?? ""
     }
-
+    
     func write(_ items: Any...) {
         items.forEach { Swift.print($0) }
     }
-
+    
     func writeErrorLine(_ errorMessage: String) {
         writeErrorLine(errorMessage, "")
     }
-
+    
     func writeErrorLine(_ errorMessage: String, _ helpMessage: String) {
         fputs("\(TextPrefix.error): \(errorMessage)\n", stderr)
         if !helpMessage.isEmpty { fputs("\(helpMessage)\n", stderr) }

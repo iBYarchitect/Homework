457ac4a3642d2e75cf7db42c72ceda81c5913009
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-03-23 09:39:08 +0100

    Adding protocol ConsoleIOWrapper

---
+++ CLIShoppingListApplication/CLIShoppingListApplication/CLIShoppingListApplication/ConsoleIOWrapper.swift
@@ -0,0 +1,33 @@
+/**
+ This protocol provides a wrapper for command-line I/O. Can read from the STDIN and write into STDOUT, STDERR.
+ */
+protocol ConsoleIOWrapper {
+    /**
+     Reads the data from standard input through the end of the current line or until EOF is reached.
+     - Returns: String read from standard input. If data not provided, returns empty string.
+     */
+    func readLine() -> String
+
+    /**
+     Writes the textual representations of the given items into the standard output.
+     - Parameter items: A sequence of elements to write into the STDOUT.
+     */
+    func write(_ items: Any...)
+
+    /**
+     Writes an error message to the standard error stream (STDERR)
+     - Parameter errorMessage: An error text message that should be written into STDERR.
+     */
+    func writeErrorLine(_ errorMessage: String)
+
+    /**
+     Writes an error message to the standard error stream (STDERR), along with an optional help message.
+     Help message writes on a new line.
+
+     - Parameters:
+         - errorMessage: A string representing the error message to be written to stderr.
+         - helpMessage: An optional string representing a help message to be written to stderr.
+     If this string is empty, it will not be written.
+     */
+    func writeErrorLine(_ errorMessage: String, _ helpMessage: String)
+}
